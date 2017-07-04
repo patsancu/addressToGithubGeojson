551ba88486a8c552cfcc68459fb3d2495e5d0354
Patrick <patrick@sanzo.es>
2017-07-04 19:07:09 +0200

    privatize methods

--- addressToGHgeojsonObject.py
+++ addressToGHgeojsonObject.py
@@ -14,20 +14,20 @@ class Address_to_gist_map():
     def initalize(self):
         self.gmaps = googlemaps.Client(key=self.google_maps_key)
 
-    def write_to_file(self, filename, contents):
+    def _write_to_file(self, filename, contents):
         f = open(filename, "w")
         f.write(contents)
         f.close()
 
-    def get_coordinate_set_from_strings(self):
-        coordinate_set = map(self.from_string_to_coordinates_and_poi, self.addresses)
+    def _get_coordinate_set_from_strings(self):
+        coordinate_set = map(self._from_string_to_coordinates_and_poi, self.addresses)
         if self.debug:
             print coordinate_set
-        self.fromCoordinatesToFeatureCollection(coordinate_set)
+        self._fromCoordinatesToFeatureCollection(coordinate_set)
         if self.debug:
             print self.fc
 
-    def from_string_to_coordinates_and_poi(self, address_string):
+    def _from_string_to_coordinates_and_poi(self, address_string):
         address_tokens = address_string.split(';')
         poi_type = None
         if len(address_tokens) > 1:
@@ -49,13 +49,13 @@ class Address_to_gist_map():
             print "address {} has coordinates: {},{}".format(address_string, lat, lng)
         return lng, lat, poi_type
 
-    def get_icon_name_for_poi_type(self, poi_type):
+    def _get_icon_name_for_poi_type(self, poi_type):
         if poi_type == "bar":
             return "bar"
         print "Poi_type is not a bar, it's a: {}".format(poi_type)
         return 'triangle'
 
-    def fromCoordinatesToFeatureCollection(self, coordinatesSet, address_string = None):
+    def _fromCoordinatesToFeatureCollection(self, coordinatesSet, address_string = None):
         feature_collection = []
         for coordinate in coordinatesSet:
             lat = coordinate[0]
@@ -65,24 +65,24 @@ class Address_to_gist_map():
                 print "poi_type is: {}".format(poi_type)
             properties = {}
             if poi_type:
-                properties['marker-symbol'] = self.get_icon_name_for_poi_type(poi_type)
+                properties['marker-symbol'] = self._get_icon_name_for_poi_type(poi_type)
             my_point = geojson.Point((lat, lng))
             my_feature = geojson.Feature(geometry = my_point, properties=properties)
             feature_collection.append(my_feature)
 
         self.fc = geojson.FeatureCollection(feature_collection)
 
-    def create_string_from_geojson(self):
+    def _create_string_from_geojson(self):
         dump = geojson.dumps(self.fc, sort_keys=True)
         return dump
 
     ### Writes string to file
-    def writeContentsToFile(self, contents, file_name):
+    def _writeContentsToFile(self, contents, file_name):
         if write_to_file_flag:
             write_to_file(filename, dump)
 
-    def createGistDictFromGeojson(self, filename, description):
-        content = self.create_string_from_geojson()
+    def _createGistDictFromGeojson(self, filename, description):
+        content = self._create_string_from_geojson()
         public = True
         gist_data = {}
         gist_data['description'] = description
@@ -93,7 +93,7 @@ class Address_to_gist_map():
         self.gist_data = gist_data
 
     # Tries to publish gist and returns response status and gist url (if successful) or blank
-    def publishToGists(self):
+    def _publishToGists(self):
         if self.github_token:
             headers =  {'Authorization': 'token {}'.format(self.github_token) }
             response = requests.post("https://api.github.com/gists", json=self.gist_data, headers=headers)
@@ -107,8 +107,8 @@ class Address_to_gist_map():
         return (response.ok, obj['html_url'])
 
     def gist_map_from_addresses(self, filename="some_map.geojson", description="some description"):
-        self.get_coordinate_set_from_strings()
-        self.createGistDictFromGeojson(filename, description)
-        response, gist_url = self.publishToGists()
+        self._get_coordinate_set_from_strings()
+        self._createGistDictFromGeojson(filename, description)
+        response, gist_url = self._publishToGists()
         print gist_url
 
